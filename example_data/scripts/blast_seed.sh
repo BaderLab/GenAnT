6ba6cd7e6b0ad374b5398856a5bb647c0c877af2
Dustin Sokolowski <56414662+DustinSokolowski@users.noreply.github.com>
2025-01-29 16:34:29 -0500

    add in ncRNA scripts

---
+++ example_data/scripts/blast_seed.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+rfamDir=$dataDir/Rfam
+
+cd $outDir
+
+mkdir -p blast_seed
+
+cd blast_seed
+
+blastn -db $rfamDir/Rfam_nodup -query $outDir/assembly/assembly.softmasked.fa -evalue 1e-6 -max_hsps 6 -max_target_seqs 6 -outfmt 6 -out assembly.Rfam.blastn -num_threads 24
+
+awk -F "\t" '{print $1 "\t" $7 "\t" $8 "\t" $2}' assembly.Rfam.blastn > assembly.rfam.bed
