0ee678f880f9d2ccbccc8b79fbc43c74904ac6d7
Dustin Sokolowski <56414662+DustinSokolowski@users.noreply.github.com>
2025-05-15 17:19:26 -0400

    Update ncbi_gene_key.R
    increase generalizability

--- setup/ncbi_gene_key.R
+++ setup/ncbi_gene_key.R
@@ -22,16 +22,10 @@ gff_transcript <- gff_transcript <- gff[gff$type == "transcript" | gff$type == "
 # Get gene ID and transcript ID keys
 # if this is a character already it doesn't change anything, but Rtracklayer sometimes loads "Parent" in as a list.
 
-gff_transcript$gene_id <- unlist(gff_transcript$Parent)
-gff_transcript$trans_id <- unlist(gff_transcript$ID)
-
 key <- gff_transcript[,c("gene_id","trans_id","gene")]
 colnames(key) <- c("geneID","transcriptID","geneSymbol")
 
-# Remove deleting the `gene-` and `rna-` tag as it makes an incompatibility with toga isoforms
-
-# key$geneID <- gsub("gene-","",key$geneID)
-# key$transcriptID <- gsub("rna-","",key$transcriptID)
+key <- apply(key,2,unlist)
 
 write.table(key[,c("geneSymbol","transcriptID")],file=paste0(prefix,".table.txt"),
             quote=FALSE,row.names=FALSE,col.names=TRUE,sep="\t")
