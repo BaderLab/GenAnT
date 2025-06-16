696bc496ebaefd3be4f5d4b3b18225a069353f77
Dustin Sokolowski <56414662+DustinSokolowski@users.noreply.github.com>
2025-06-16 17:20:28 -0400

    Delete scripts directory
    to be updated with generic scripts after revisions

--- scripts/run_interproscan.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd $outDir
-
-export PATH="$externalDir/my_interproscan/interproscan-5.69-101.0:$PATH"
-
-mkdir -p interproscan ; cd interproscan
-
-interproscan.sh -i $outDir/transcript_selection/mikado_lenient.faa -f tsv -dp
--- scripts/run_transdecoder.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir -p $outDir/transcript_selection/transdecoder
-
-cd $outDir/transcript_selection/transdecoder
-
-TransDecoder.LongOrfs -t $outDir/transcript_selection/mikado_prepared.fasta -m 100 -O "transdecoder"
-TransDecoder.Predict -t $outDir/transcript_selection/mikado_prepared.fasta --retain_long_orfs_length 100 -O "transdecoder"
