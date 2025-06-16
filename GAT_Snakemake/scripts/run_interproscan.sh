d5c3fa9ce28731208bc938f8267ae00a5b39427d
Dustin Sokolowski <56414662+DustinSokolowski@users.noreply.github.com>
2025-06-16 16:38:07 -0400

    Delete GAT_Snakemake directory
    To update with Snakemake directory after revisions

--- GAT_Snakemake/scripts/run_interproscan.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd $outDir
-
-mkdir -p interproscan ; cd interproscan
-
-$externalDir/my_interproscan/interproscan-5.69-101.0/interproscan.sh -i $outDir/transcript_selection/mikado_lenient.faa -f tsv -dp
