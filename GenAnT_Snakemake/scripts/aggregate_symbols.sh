72a4b2eef22eef4d5545894ea511636f6f421b07
Dustin Sokolowski <56414662+DustinSokolowski@users.noreply.github.com>
2025-07-06 16:07:07 -0400

    bug fixes - exon gene matching
    switched gene symbol assignment for liftOff and TOGA from mRNA and exon. fixing some bugs introduced from the switch

--- GenAnT_Snakemake/scripts/aggregate_symbols.sh
+++ GenAnT_Snakemake/scripts/aggregate_symbols.sh
@@ -3,6 +3,8 @@
 outDir=$1
 orthofinderTab=$2
 snakeDir=$3
+refTogaIsoform=$4
+refTogaIsoform2=$5
 
 
 cd $outDir
@@ -34,46 +36,50 @@ fi
 #
 
 # mikado
-grep -P "\tmRNA\t|\tlncRNA\t" full_annotation.gff > mikado.mRNA.lncRNA.gff
+grep -P "\texon\t" full_annotation.gff > mikado.exon.gff
 
 # liftoff
-grep -P "\tmRNA\t|\tlnc_RNA\t|\ttranscript\t" liftoff.gffread.gff > liftoff.mRNA.lncRNA.gff
+grep -P "\texon\t" liftoff.gffread.gff > liftoff.exon.gff
 
 # toga.r1
-grep -P "\tmRNA\t|\tlnc_RNA\t|\ttranscript\t" toga.r1.gffread.gff > toga.r1.mRNA.lncRNA.gff
+grep -P "\texon\t" toga.r1.gffread.gff > toga.r1.exon.gff
 
 # toga.r2
 
-grep -P "\tmRNA\t|\tlnc_RNA\t|\ttranscript\t" toga.r2.gffread.gff > toga.r2.mRNA.lncRNA.gff
+grep -P "\texon\t" toga.r2.gffread.gff > toga.r2.exon.gff
 
 # liftoff
-bedtools intersect -a mikado.mRNA.lncRNA.gff -b liftoff.mRNA.lncRNA.gff -wo > mikado.liftoff.mRNA.lncRNA.txt
+bedtools intersect -a mikado.exon.gff -b liftoff.exon.gff -wo > mikado.liftoff.exon.txt
 
 # toga.r1
-bedtools intersect -a mikado.mRNA.lncRNA.gff -b toga.r1.mRNA.lncRNA.gff -wo > mikado.toga.r1.mRNA.lncRNA.txt
+bedtools intersect -a mikado.exon.gff -b toga.r1.exon.gff -wo > mikado.toga.r1.exon.txt
 
 # toga.r2
-bedtools intersect -a mikado.mRNA.lncRNA.gff -b toga.r2.mRNA.lncRNA.gff -wo > mikado.toga.r2.mRNA.lncRNA.txt
+bedtools intersect -a mikado.exon.gff -b toga.r2.exon.gff -wo > mikado.toga.r2.exon.txt
 
 
 # Liftoff
 
-cut -f1-9 mikado.liftoff.mRNA.lncRNA.txt > liftoff_overlap.mikadoInfo.gff
-cut -f10-18 mikado.liftoff.mRNA.lncRNA.txt > liftoff_overlap.liftoffInfo.gff
+cut -f1-9 mikado.liftoff.exon.txt > liftoff_overlap.mikadoInfo.gff
+cut -f10-18 mikado.liftoff.exon.txt > liftoff_overlap.liftoffInfo.gff
 
 # TOGA (r1)
 
-cut -f1-9 mikado.toga.r1.mRNA.lncRNA.txt > toga_overlap.r1.mikadoInfo.gff
-cut -f10-18 mikado.toga.r1.mRNA.lncRNA.txt > toga_overlap.r1.togaInfo.gff
+cut -f1-9 mikado.toga.r1.exon.txt > toga_overlap.r1.mikadoInfo.gff
+cut -f10-18 mikado.toga.r1.exon.txt > toga_overlap.r1.togaInfo.gff
 
 # TOGA (r2)
-cut -f1-9 mikado.toga.r2.mRNA.lncRNA.txt > toga_overlap.r2.mikadoInfo.gff
-cut -f10-18 mikado.toga.r2.mRNA.lncRNA.txt > toga_overlap.r2.togaInfo.gff
+cut -f1-9 mikado.toga.r2.exon.txt > toga_overlap.r2.mikadoInfo.gff
+cut -f10-18 mikado.toga.r2.exon.txt > toga_overlap.r2.togaInfo.gff
 
 # Copy orthofinder output over
 
 cp $outDir/orthofinder/orthofinder_protein.tsv ./
 cp $orthofinderTab ./reference.table.txt
+
+cp $refTogaIsoform ./reference.toga.r1.table.txt
+cp $refTogaIsoform2 ./reference.toga.r2.table.txt
+
 
 # Start the gene symbol table and add in results from LiftOff and TOGA
 
