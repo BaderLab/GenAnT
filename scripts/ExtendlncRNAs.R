ecea195a416b399dd2e512569cbe4919616a864f
Dustin Sokolowski <56414662+DustinSokolowski@users.noreply.github.com>
2025-11-12 13:02:00 -0500

    Update ExtendlncRNAs.R

--- scripts/ExtendlncRNAs.R
+++ scripts/ExtendlncRNAs.R
@@ -22,45 +22,50 @@ head(mikado)
 
 # Now read in Infernal lncRNA information 
 
-infernal <- BiocGenerics::as.data.frame(read.delim("infernal.lncRNA.InMikado.gff",header=FALSE,as.is=TRUE))
-infernal <- infernal[infernal$V12 != "superlocus",]
-infernal <- infernal[infernal$V7 == infernal$V16,] # redundant, ensure strand
-infernal <- infernal[infernal$V1 == infernal$V10,] # redundant, ensure chr
-# Get Infernal IDs with a strand-specific overlap to the lncRNA
+infernal <- try(BiocGenerics::as.data.frame(read.delim("infernal.lncRNA.InMikado.gff",header=FALSE,as.is=TRUE)))
 
-
-infernalID <- gsub("ID=","",unlist(lapply(strsplit(infernal$V18,";"),function(x) return(x[1]))))
-
-# Get other relevant infernal information to add to mikado gff
-thesplit <- as.data.frame(do.call("rbind",strsplit(infernal$V9,";")))
-colnames(thesplit) <- c("ID","evalue","rfamid","product","gbkey")
-for(i in colnames(thesplit)) {
-  thesplit[,i] <- gsub(paste0(i,"="),"",thesplit[,i])
-}
-thesplit$product <- gsub("\t", " ", thesplit$product)
-
-for(i in 1:length(infernalID)) {
-  if(infernal$V4[i] < infernal$V13[i])  { # extend to the left 
-    mikado[mikado$ID == infernalID[i],"start"] <- infernal$V4[i]
-    message(paste0(infernalID[i], " is extended from Infernal lncRNA at start"))
+if(class(infernal)[[1]] != "try-error") {
+  
+  infernal <- infernal[infernal$V12 != "superlocus",]
+  infernal <- infernal[infernal$V7 == infernal$V16,] # redundant, ensure strand
+  infernal <- infernal[infernal$V1 == infernal$V10,] # redundant, ensure chr
+  # Get Infernal IDs with a strand-specific overlap to the lncRNA
+  
+  
+  infernalID <- gsub("ID=","",unlist(lapply(strsplit(infernal$V18,";"),function(x) return(x[1]))))
+  
+  # Get other relevant infernal information to add to mikado gff
+  thesplit <- as.data.frame(do.call("rbind",strsplit(infernal$V9,";")))
+  colnames(thesplit) <- c("ID","evalue","rfamid","product","gbkey")
+  for(i in colnames(thesplit)) {
+    thesplit[,i] <- gsub(paste0(i,"="),"",thesplit[,i])
   }
-  if(infernal$V5[i] > infernal$V15[i])  { # extend to the right 
-    mikado[mikado$ID == infernalID[i],"end"] <- infernal$V5[i]
-    message(paste0(infernalID[i], " is extended from Infernal lncRNA at end"))
-    
-  }
-  if(infernal$V12[i] %in% c("gene","mRNA","ncRNA_gene","ncRNA")) {
-    
-    # Add in correct metadata
-    mikado$predicted_gene_symbol[i] <- thesplit$ID[i]
-    mikado$infernal_product[i] <- thesplit$product[i]
-    if(mikado$type[i] %in% c("gene","lncRNA_gene")) {
-      mikado$gene_biotype[i] <- "lncRNA"
-    } else {
-      mikado$gbkey[i] <- "ncRNA"
+  thesplit$product <- gsub("\t", " ", thesplit$product)
+  
+  for(i in 1:length(infernalID)) {
+    if(infernal$V4[i] < infernal$V13[i])  { # extend to the left 
+      mikado[mikado$ID == infernalID[i],"start"] <- infernal$V4[i]
+      message(paste0(infernalID[i], " is extended from Infernal lncRNA at start"))
+    }
+    if(infernal$V5[i] > infernal$V15[i])  { # extend to the right 
+      mikado[mikado$ID == infernalID[i],"end"] <- infernal$V5[i]
+      message(paste0(infernalID[i], " is extended from Infernal lncRNA at end"))
+      
+    }
+    if(infernal$V12[i] %in% c("gene","mRNA","ncRNA_gene","ncRNA")) {
+      
+      # Add in correct metadata
+      mikado$predicted_gene_symbol[i] <- thesplit$ID[i]
+      mikado$infernal_product[i] <- thesplit$product[i]
+      if(mikado$type[i] %in% c("gene","lncRNA_gene")) {
+        mikado$gene_biotype[i] <- "lncRNA"
+      } else {
+        mikado$gbkey[i] <- "ncRNA"
+      }
     }
   }
 }
+                                          
 
 rtracklayer::export.gff3(mikado, "mikado.infernal.lncRNALabeled.polished.gff", format = "gff3")
 
